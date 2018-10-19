5359624feb5fe42a5ed02a45929c237be346ca9b
Alex <Alex@DESKTOP-KH2V179>
2018-10-19 17:54:20 +0200

    CONEXIO FAIL

--- ProjecteBiblioteca/test/Form1.cs
+++ ProjecteBiblioteca/test/Form1.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace test {
-    public partial class Form1 : Form {
-        public Form1() {
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=DESKTOP-FHEALRN;Initial Catalog=Biblioteca;User ID=sa;Password=edul23";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
         }
     }
 }
