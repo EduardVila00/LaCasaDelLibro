07827895a3df98c3dc1f38f3adc4efe479f4667d
Eduard Vila <Eduard Vila@DESKTOP-FHEALRN>
2018-10-19 18:44:20 +0200

    Pas 5 MVC

---
+++ ProjecteBiblioteca/View/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace View {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dgvLlibres = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLlibres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLlibres
+            // 
+            this.dgvLlibres.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLlibres.Location = new System.Drawing.Point(12, 12);
+            this.dgvLlibres.Name = "dgvLlibres";
+            this.dgvLlibres.Size = new System.Drawing.Size(240, 150);
+            this.dgvLlibres.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvLlibres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLlibres)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLlibres;
+    }
+}
+
