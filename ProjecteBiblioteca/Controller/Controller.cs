105dc9ee3890d8747612b28297928939855ce591
Alex <Alex@DESKTOP-KH2V179>
2018-10-24 19:51:41 +0200

    Created Controller

---
+++ ProjecteBiblioteca/Controller/Controller.cs
@@ -0,0 +1,34 @@
+﻿using Model;
+using View;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class Controller
+    {
+        View.Menu menu = new View.Menu();
+        BibliotecaEntities db = new BibliotecaEntities();
+
+
+        public void init()
+        {
+            initListeners();
+            run();
+        }
+
+        public void initListeners()
+        {
+
+        }
+
+        public void run()
+        {
+            Application.Run(menu);
+        }
+    }
+}
